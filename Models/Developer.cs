e8150b12825c7df1f5bd6029a2bfb919a04c8872
Ahmed Taher <90255766+ahmedtaher45@users.noreply.github.com>
2025-07-12 15:55:45 +0300

    Add project files.

---
+++ Models/Developer.cs
@@ -0,0 +1,9 @@
+﻿namespace GenAI_Recommendation_Model.Models
+{
+    public class Developer
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public string History { get; set; }
+    }
+}
